970b0f0e47808fe7c85e5584600341181493b0d8
ankan112 <ankanhalder112@gmail.com>
2024-01-02 00:52:11 +0600

    problem 1 is solved

--- bootcamp-problem-day-1/app.js
+++ bootcamp-problem-day-1/app.js
@@ -1,2 +1,7 @@
 /* Problem 1 */
+
+const stringOutput = `Recently I heard that you’ve achieved 95% marks in your exam.\nThis is brilliant!\nI wish you’ll shine in your life!\xa0\xa0\xa0\xa0\xa0\xa0\xa0Good luck with all the barriers(/\\) in your life.`;
+console.log(stringOutput);
+
+/* Problem 2 */
 const arr = [1, 5, 9, 14, 5, 22, 48, 25, 22, 20, 9, 13];
