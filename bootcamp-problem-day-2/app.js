905b81dc49e9a4ee638f30ebd74efa59389ae380
ankan112 <ankanhalder112@gmail.com>
2024-01-23 02:05:40 +0600

    another problem solved

--- bootcamp-problem-day-2/app.js
+++ bootcamp-problem-day-2/app.js
@@ -1,1 +1,9 @@
 /* Problem 1 */
+// 2. Task: Leap Year Checker
+// Write a function that determines whether a given year is a leap year.
+
+function isLeapYear(year) {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
+
+console.log("isLeapYear", isLeapYear(2024));
