e50904f4501e3d209a19dccdfc042da49424a213
ankan112 <ankanhalder112@gmail.com>
2024-01-25 22:07:40 +0600

    file update

--- bootcamp-problem-day-2/app.js
+++ bootcamp-problem-day-2/app.js
@@ -6,4 +6,4 @@ function isLeapYear(year) {
   return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
 
-console.log("isLeapYear", isLeapYear(2024));
+console.log("isLeapYear", isLeapYear(2040));
