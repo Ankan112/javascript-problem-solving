5708df46dc50127526d177231ff58e54b49463b3
ankan112 <ankanhalder112@gmail.com>
2024-01-26 18:01:29 +0600

    task two done

--- bootcamp-task/app.js
+++ bootcamp-task/app.js
@@ -11,3 +11,25 @@ const people = [
 const males = people.filter((person) => person.gender === "male");
 const names = males.map((person) => person.name);
 // console.log(names); // Output
+
+//! Task: Object Manipulation
+// Create an array of objects representing books with properties like title, author, and year. Write a function that takes the array and returns a new array with only the book titles. Print the result.
+
+const books = [
+  { title: "The Lord of the Rings", author: "J.R.R. Tolkien", year: 1954 },
+  { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+  {
+    title: "One Hundred Years of Solitude",
+    author: "Gabriel García Márquez",
+    year: 1967,
+  },
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+];
+
+function extractTitles(bookList) {
+  return bookList.map((book) => book.title);
+}
+
+const bookTitles = extractTitles(books);
+console.log(bookTitles); // Output
