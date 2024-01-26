593b9c57adb201570e0ddd7d58659da101eb78df
ankan112 <ankanhalder112@gmail.com>
2024-01-26 18:22:47 +0600

    task seven done

--- bootcamp-task/app.js
+++ bootcamp-task/app.js
@@ -116,3 +116,11 @@ function sumEvenNumbers(numArray) {
 
 const sumOfEvens = sumEvenNumbers(numbers);
 // console.log("Sum of even numbers:", sumOfEvens); //Output
+
+//! Task: Leap Year Checker
+// Write a function that determines whether a given year is a leap year.
+function isLeapYear(year) {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
+
+// console.log("isLeapYear", isLeapYear(2040)); //Output
