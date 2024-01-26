a417fa24d55fb914e244f6e5331ba5c296aba63c
ankan112 <ankanhalder112@gmail.com>
2024-01-26 18:13:50 +0600

    task four done

--- bootcamp-task/app.js
+++ bootcamp-task/app.js
@@ -54,4 +54,21 @@ const combinedFunction = (x) => addFive(double(square(x)));
 const number = 2;
 const result = combinedFunction(number);
 
-console.log(`Number: ${number}, Result: ${result}`); // Output
+// console.log(`Number: ${number}, Result: ${result}`); // Output
+
+//! Task: Sorting Objects
+// Create an array of objects representing cars with properties like make, model, and year. Write a function to sort the array of cars by the year of manufacture in ascending order. Print the sorted array.
+
+const cars = [
+  { make: "Toyota", model: "Camry", year: 2023 },
+  { make: "Honda", model: "Civic", year: 2020 },
+  { make: "Ford", model: "Mustang", year: 2018 },
+  { make: "Chevrolet", model: "Corvette", year: 2022 },
+];
+
+function sortCarsByYear(carList) {
+  return carList.sort((carA, carB) => carA.year - carB.year);
+}
+
+const sortedCars = sortCarsByYear(cars);
+// console.log(sortedCars); //Output
